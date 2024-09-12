2a8663f69db5341b28aa1056f7491e61a195c8a8
khali <khali@KHALID>
2024-09-12 20:24:17 +0600

    Add project files.

---
+++ MyFirstProject/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace MyFirstProject
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginPage));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.txtMessage1 = new System.Windows.Forms.TextBox();
+            this.txtMessage2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnMult = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnPlus.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPlus.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlus.Location = new System.Drawing.Point(320, 178);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(75, 44);
+            this.btnPlus.TabIndex = 5;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = false;
+            // 
+            // txtMessage1
+            // 
+            this.txtMessage1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtMessage1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMessage1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtMessage1.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessage1.Location = new System.Drawing.Point(480, 46);
+            this.txtMessage1.Name = "txtMessage1";
+            this.txtMessage1.Size = new System.Drawing.Size(272, 28);
+            this.txtMessage1.TabIndex = 6;
+            // 
+            // txtMessage2
+            // 
+            this.txtMessage2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtMessage2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMessage2.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessage2.Location = new System.Drawing.Point(480, 100);
+            this.txtMessage2.Name = "txtMessage2";
+            this.txtMessage2.Size = new System.Drawing.Size(272, 28);
+            this.txtMessage2.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(315, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 29);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "TextBox1 :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(315, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 29);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "TextBox2 :";
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnMinus.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMinus.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinus.Location = new System.Drawing.Point(424, 178);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(75, 44);
+            this.btnMinus.TabIndex = 10;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = false;
+            // 
+            // btnMult
+            // 
+            this.btnMult.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnMult.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMult.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMult.Location = new System.Drawing.Point(526, 178);
+            this.btnMult.Name = "btnMult";
+            this.btnMult.Size = new System.Drawing.Size(75, 44);
+            this.btnMult.TabIndex = 11;
+            this.btnMult.Text = "X";
+            this.btnMult.UseVisualStyleBackColor = false;
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnDiv.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDiv.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDiv.Location = new System.Drawing.Point(627, 178);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(75, 44);
+            this.btnDiv.TabIndex = 12;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = false;
+            // 
+            // LoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(832, 453);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMult);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMessage2);
+            this.Controls.Add(this.txtMessage1);
+            this.Controls.Add(this.btnPlus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginPage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.TextBox txtMessage1;
+        private System.Windows.Forms.TextBox txtMessage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnMult;
+        private System.Windows.Forms.Button btnDiv;
+    }
+}
+
---
+++ MyFirstProject/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstProject
+{
+    public partial class LoginPage : Form
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            btnPlus.Tag = "Add";
+            btnMinus.Tag = "Subtract";
+            btnMult.Tag = "Multiply";
+            btnDiv.Tag = "Divide";
+
+            btnPlus.Click += btnOperation_Click;
+            btnMinus.Click += btnOperation_Click;
+            btnMult.Click += btnOperation_Click;
+            btnDiv.Click += btnOperation_Click;
+        }
+        private void btnOperation_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string txt1 = txtMessage1.Text;
+                string txt2 = txtMessage2.Text;
+                double num1 = double.Parse(txt1);
+                double num2 = double.Parse(txt2);
+                double result = 0;
+
+                // Get the operation from the button's Tag property
+                Button clickedButton = sender as Button;
+                string operation = clickedButton.Tag.ToString();
+
+                switch (operation)
+                {
+                    case "Add":
+                        result = num1 + num2;
+                        break;
+                    case "Subtract":
+                        result = num1 - num2;
+                        break;
+                    case "Multiply":
+                        result = num1 * num2;
+                        break;
+                    case "Divide":
+                        if (num2 == 0)
+                        {
+                            throw new DivideByZeroException();
+                        }
+                        result = num1 / num2;
+                        break;
+                }
+
+                MessageBox.Show("Result: " + result.ToString());
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
